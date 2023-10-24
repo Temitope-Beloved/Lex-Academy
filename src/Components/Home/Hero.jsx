67400f1a4bf8d125398382381d86991450998baf
Temitope-Beloved <olafusisam236@gmail.com>
2023-10-23 19:03:06 -0700

    New folder Structure

--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -6,12 +6,12 @@ import homepage3 from '../assets/homepage3.png'
 const Hero = () => {
   return (
     <div >
-      <div className='flex justify-around   '>
+      <div className='flex justify-between  items-center mx-16 pt-7'>
         <div className='text-left teacking-wider  '>
-          <h2 className='font-semibold tracking-wider leading-14 text-5xl lg:w-[400px] md:w-[400px] pb-8    lg:text-5xl'>Elevate your learning  with our Tech course</h2>
+          <h2 className='font-semibold tracking-wider leading-14 text-5xl lg:w-[600px] md:w-[400px] pb-8    lg:text-6xl'>Elevate your learning  with our Tech course</h2>
          
           <p className='text-left text-md tracking-wider leading-relaxed max-w-[450px]  font-poppins pb-4 text-[#444]'>Joining Lex Design Academy is the parfect way to learn any skill, anywhere. With our state-of-the-art learnig tools and resources, you can master any skill at your own pace</p>
-          <button className='bg-secondary rounded-md text-white text-lg font-semibold  p-3'>Start learning now</button>
+          <button className='bg-secondary rounded-md text-white text-lg lg:text-2xl font-semibold  lg:p-4 hover:bg-white hover:text-secondary border-2 border-secondary ease-out duration-300'>Start learning now</button>
         </div>
         
         <div className='flex flex-row items-center gap-3 '>
